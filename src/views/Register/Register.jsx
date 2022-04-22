a7973ee03794eb716c44d9f83ae004f6b1f7c248
Pablo Mateos <pmateos805@gmail.com>
2022-04-22 21:00:31 +0200

    feat: register validations

--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -13,7 +13,7 @@ const schema = yup.object({
 }).required()
 
 const Register = () => {
-    const [ backErrors, setBackErros ] = useState({})
+    const [ backErrors, setBackErrors ] = useState({})
     const [ isSubmitting, setIsSubmitting ] = useState(false)
     const navigate = useNavigate()
     const { register, handleSubmit, formState:{ errors } } = useForm({
@@ -21,26 +21,26 @@ const Register = () => {
     })
 
     const onSubmit = (data) => {
-        setBackErros({})
+        setBackErrors({})
         setIsSubmitting(true)
 
         createUser(data)
             .then((userCreated) => {
                 navigate('/login')
             })
-            .catch(err => setBackErros(err?.response?.data?.erorrs))
+            .catch(err => {
+                setBackErrors(err?.response?.data)
+            })
             .finally(() => {
                 setIsSubmitting(false)
             })
-
-
     }
 
     return (
         <div className='container Register'>
             <div className='row'>
                 <div className='col-lg-6'>
-                Aqui cosas
+                <img src="https://public-files.gumroad.com/variants/sxn5t8ady8tbj4yeymyh5vi6d9f2/4d4cb575366005157970186bb171da8f6b6b8bb857dcdd1f8e93774cc5f0900d" alt="" />
                 </div>
                 <div className='col-lg-4'>
                     <h2 className='title'>Welcome to MoneyControl</h2>
@@ -53,16 +53,33 @@ const Register = () => {
                         <form onSubmit={handleSubmit(onSubmit)} className='form-container'>
                             <div className='formField'>
                                 <label>Name</label>
-                                <input type="text" />
+                                <input
+                                 className={errors.name?.message ? 'invalid' : ''}
+                                {...register('name')} 
+                                type="text" 
+                                />
+                                {errors.name && <small style={{color: "red"}}>{errors.name?.message}</small>}
                             </div>
                             <div className='formField'>
                                 <label>Email</label>
-                                <input type="email" />
+                                <input
+                                 className={`backErrors || errors.name?.message ? 'invalid' : 'valid'`}
+                                {...register('email')} 
+                                type="email" 
+                                />
+                                {errors.email && <small style={{color: "red"}}>{errors.email?.message}</small>}
                             </div>
                             <div className='formField'>
                                 <label>Password</label>
-                                <input type="password" />
+                                <input
+                                className={`backErrors?.password || errors.name?.message ? 'invalid' : 'valid'`}
+                                {...register('password')} 
+                                type="password" 
+                                />
+                                {errors.password && <small style={{color: "red"}}>{errors.password?.message}</small>}
                             </div>
+
+                            {backErrors.message && <p style={{color: "red"}}>{backErrors.message}</p>}
 
                             <button>Create an acount</button>
                         </form>
