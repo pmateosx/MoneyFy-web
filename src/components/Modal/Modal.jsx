9314e83ad2136ac69c572e33ff8c2ecac63605cb
Pablo Mateos <pmateos805@gmail.com>
2022-04-27 13:10:44 +0200

    feat: modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,22 @@
+import './Modal.scss'
+
+import { FiXCircle } from "react-icons/fi";
+import { useRef } from 'react';
+
+const Modal = ({children, onClose, title}) => {
+    const modalRef = useRef()
+
+    const closeOutModal = e => modalRef.current === e.target ? onClose() : ''
+
+    return (
+        <div className='Modal' ref={modalRef} onClick={closeOutModal}>
+            <div className='modal-content'>
+                <button onClick={onClose}><FiXCircle/></button>
+                <h3>{title}</h3>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+export default Modal
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom'
 import { useAuthContext } from '../../contexts/AuthContext';
 import { logout } from '../../store/AccessTokenStore.js'
+import Modal from '../Modal/Modal';
 
 import { FiPlus } from "react-icons/fi";
 
@@ -8,29 +10,43 @@ import './Navbar.scss'
 
 const Navbar = () => {
     const { user } = useAuthContext();
+
+    const [showModal, setShowModal] = useState(false)
+
+    const handleOpenModal = () => {
+        setShowModal(true)
+    }
+    const handleCloseModal = () => {
+        setShowModal(false)
+    }
+
     return (
-        <div className='navbar'>
-            <NavLink to='/home'>Logo</NavLink>
-            <div className='flexy'>
-                {user && <button className='add-btn' onClick={()=>''}> <FiPlus/></button>}
-                {user ? 
-                (
-                    <NavLink to='/profile' className='flexy navUser'>
-                        <div className='frame-img'>
-                        <img src={user.avatar} alt="user avatar" />
-                        </div>
-                        <p>Hi, {user.name}</p> 
-                    </NavLink>
-                )
-                :
-                (<div className='flexy'>
-                    <NavLink to='/login'>Login</NavLink>
-                    <NavLink to='/register'>Register</NavLink>
-                </div>)
-                }
-                {user && <button className='button-out' onClick={()=>logout()}>Logout</button>}
+        <>
+            <div className='navbar'>
+                <NavLink to='/home'>Logo</NavLink>
+                <div className='flexy'>
+                    {user && <button className='add-btn' onClick={handleOpenModal}> <FiPlus/></button>}
+                    {user ? 
+                    (
+                        <NavLink to='/profile' className='flexy navUser'>
+                            <div className='frame-img'>
+                            <img src={user.avatar} alt="user avatar" />
+                            </div>
+                            <p>Hi, {user.name}</p> 
+                        </NavLink>
+                    )
+                    :
+                    (<div className='flexy'>
+                        <NavLink to='/login'>Login</NavLink>
+                        <NavLink to='/register'>Register</NavLink>
+                    </div>)
+                    }
+                    {user && <button className='button-out' onClick={()=>logout()}>Logout</button>}
+                </div>
             </div>
-        </div>
+            {showModal && <Modal onClose={handleCloseModal} title='Hola que tal'> Hola </Modal>}
+        </>
+
     )
 }
 
