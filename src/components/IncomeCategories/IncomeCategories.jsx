278fefb2f965ed74f6ab227559e510bf191ba0fe
Pablo Mateos <pmateos805@gmail.com>
2022-05-02 16:50:32 +0200

    fix: categories module styled & total incomes

--- src/components/IncomeCategories/IncomeCategories.jsx
+++ src/components/IncomeCategories/IncomeCategories.jsx
@@ -1,33 +1,37 @@
+import { useEffect, useState } from 'react'
+import { useAuthContext } from '../../contexts/AuthContext'
 import './IncomeCategories.scss'
 import IncomeCategoriesChart from './IncomeCategoriesChart/IncomeCategoriesChart'
 
 const IncomeCategories = () => {
+    const {user} = useAuthContext()
+    const [totalIncomes, setTotalIncomes] = useState()
+
+    useEffect(() => {
+        let incomeRegistered = user?.income.length
+        setTotalIncomes(incomeRegistered)
+    }, [user?.income])
+    
     return (
         <div className='IncomeCategories'>
-            <div>
-            <div>
-                <h2>26</h2>
-                <h4>Nº of incomes</h4>
-            </div>
-                <h3>Legend</h3>
+            <div className='content'>
+                <h2>{totalIncomes > 0 && totalIncomes + ' incomes'}</h2>
                 <div>
-                    <div>
-                        <div className='legend'>
-                            <span className='circle'></span>
-                            <h4>Incomes</h4>
-                            <span className='circle'></span>
-                            <h4>Incomes</h4>
-                            <span className='circle'></span>
-                            <h4>Incomes</h4>
-                            <span className='circle'></span>
-                            <h4>Incomes</h4>
-                            <span className='circle'></span>
-                            <h4>Incomes</h4>
-                            <span className='circle'></span>
-                            <h4>Incomes</h4>
-                        </div>
+                    <h3>Categories</h3>
+                    <div className='legend'>
+                        <span className='circle'></span>
+                        <h4>Incomes</h4>
+                        <span className='circle'></span>
+                        <h4>Incomes</h4>
+                        <span className='circle'></span>
+                        <h4>Incomes</h4>
+                        <span className='circle'></span>
+                        <h4>Incomes</h4>
+                        <span className='circle'></span>
+                        <h4>Incomes</h4>
+                        <span className='circle'></span>
+                        <h4>Incomes</h4>
                     </div>
-                    
                 </div>
             </div>
             <IncomeCategoriesChart /> 
